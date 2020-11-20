382b1ae312574ca37d5cbadb66cc9701f36c5251
Agustin <jagustinbassi@gmail.com>
2020-11-20 17:24:03 -0300

    Add features to send post|put|delete CoAP methods and parse CoAP error codes

--- py-http-coap/src/app.py
+++ py-http-coap/src/app.py
@@ -6,6 +6,7 @@
 
 #########[ Imports ]########################################################### 
 
+
 import os
 import json
 import time
@@ -15,6 +16,7 @@ from subprocess import Popen, PIPE
 
 from flask import Flask, Response, abort, json, jsonify, request, url_for
 from flask_cors import CORS
+
 
 #########[ Settings & Data ]###################################################
 
@@ -70,9 +72,7 @@ def show_resources():
 @app.route(APP_CONFIG["PREFIX"] + '/http_to_coap/', methods=['PUT', 'POST'])
 def execute_coap_request():
     if not request.json:
-        response = {
-            "error" : "Impossible to parse request body"
-        }
+        response = generate_invalid_coap_response()
         return create_json_response(response, 422)
     # Send new current module data as response
     coap_fields = create_coap_fields_from_http_request(**request.json)
@@ -83,14 +83,6 @@ def execute_coap_request():
 
 
 def create_coap_fields_from_http_request(**kwargs):
-    """
-    coap_fields = {
-        "coap_server" : "192.168.1.37",
-        "coap_port" : 5683,
-        "coap_resource" : "button",
-        "coap_method" : "get",
-    }
-    """
     return {
         "coap_server" : kwargs.get("coap_server", "INVALID"),
         "coap_port" : int(kwargs.get("coap_port", 0)),
@@ -101,9 +93,6 @@ def create_coap_fields_from_http_request(**kwargs):
 
 
 def execute_coap_client_request(**kwargs):
-    """
-    docker-compose run py-http-coap coap-client -m get -p 5683 coap://192.168.1.37/button
-    """
 
     def _validate_fields():
         if kwargs.get('coap_server') == "INVALID":
@@ -114,13 +103,30 @@ def execute_coap_client_request(**kwargs):
             return False
         if kwargs.get('coap_method') == "invalid":
             return False
+        if kwargs.get('coap_method') in ["put", "post"]:
+            if not kwargs.get('coap_payload'):
+                return False
         return True
 
-    def _create_get_command_list():
+    def _create_get_or_delete_command_list(method="get"):
         command_list = []
         command_list.append("coap-client")
         command_list.append("-m")
-        command_list.append("get")
+        command_list.append(method)
+        command_list.append("-p")
+        command_list.append(f"{kwargs.get('coap_port')}")
+        connection = f"coap://{kwargs.get('coap_server')}/{kwargs.get('coap_resource')}"
+        command_list.append(connection)
+        return command_list
+
+    def _create_put_or_post_command_list(method="put"):
+        command_list = []
+        command_list.append("coap-client")
+        command_list.append("-m")
+        command_list.append(method)
+        command_list.append("-e")
+        payload = json.dumps(kwargs.get('coap_payload')).replace(" ", "")
+        command_list.append(payload)
         command_list.append("-p")
         command_list.append(f"{kwargs.get('coap_port')}")
         connection = f"coap://{kwargs.get('coap_server')}/{kwargs.get('coap_resource')}"
@@ -131,10 +137,13 @@ def execute_coap_client_request(**kwargs):
         return generate_invalid_coap_response(**kwargs)
 
     command_list = []
-    if kwargs.get('coap_method') == "get":
-        command_list = _create_get_command_list()
+    if kwargs.get('coap_method') in ["get", "delete"]:
+        command_list = _create_get_or_delete_command_list(method=kwargs.get('coap_method'))
+    elif kwargs.get('coap_method') in ["put", "post"]:
+        command_list = _create_put_or_post_command_list(method=kwargs.get('coap_method'))
     else:
         print(f"Unsupported CoAP method")
+        return generate_invalid_coap_response(**kwargs)
 
     command_output = subprocess.Popen(
         command_list, 
@@ -151,13 +160,61 @@ def generate_invalid_coap_response(**kwargs):
     return {
         "error" : "invalid data received from client",
         "received" : kwargs,
-        "solution" : "pass correct message like: 'coap-client -m get -p 5683 coap://192.168.1.37/button'",
+        "solution" : "pass correct message request body in JSON format",
+        "request_body_example": {
+            "coap_server" : "192.168.1.37",
+            "coap_port" : 5683,
+            "coap_resource" : "light",
+            "coap_method" : "put",
+            "coap_payload" : {
+                "light": False
+            }
+        }
     }
 
 
 def parse_coap_client_response(command_output):
+    
+    def _process_status_message(message):
+        coap_status_message = {
+            "2.00" : "OK",
+            "2.01" : "Created",
+            "2.02" : "Deleted",
+            "2.03" : "Valid",
+            "2.04" : "Changed",
+            "2.05" : "Content",
+            "2.31" : "Continue",
+            "4.00" : "Bad Request",
+            "4.01" : "Unauthorized",
+            "4.02" : "Bad Option",
+            "4.03" : "Forbidden",
+            "4.04" : "Not Found",
+            "4.05" : "Method Not Allowed",
+            "4.06" : "Not Acceptable",
+            "4.08" : "Request Entity Incomplt.",
+            "4.12" : "Precondition Failed",
+            "4.13" : "Request Ent. Too Large",
+            "4.15" : "Unsupported Content-Fmt.",
+            "5.00" : "Internal Server Error",
+            "5.01" : "Not Implemented",
+            "5.02" : "Bad Gateway",
+            "5.03" : "Service Unavailable",
+            "5.04" : "Gateway Timeout",
+            "5.05" : "Proxying Not Supported",
+        }
+        status_response = {
+            "coap_status_response": message,
+            "coap_status_description": coap_status_message.get(message, "")
+        }
+        return status_response
+        
     splitted = command_output.split("\n")
-    message_output = splitted[1]
+    try:
+        message_output = json.loads(splitted[1])
+        if isinstance(message_output, (int, float)):
+            message_output = _process_status_message(str(message_output))
+    except:
+        message_output = splitted[1]
     total_info = splitted[0].split(" ")
     version = total_info[0].split(":")[1]
     message_type = total_info[1].split(":")[1]
@@ -167,7 +224,7 @@ def parse_coap_client_response(command_output):
         "version": version,
         "message_type": message_type,
         "method": method,
-        "message_id": message_id,
+        "message_id": f"0x{message_id}",
         "output" : message_output,
     }
 
