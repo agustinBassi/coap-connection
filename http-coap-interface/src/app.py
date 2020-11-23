5acb7eb85085702b2120ffa54dbbf7d32735e611
Agustin <jagustinbassi@gmail.com>
2020-11-23 15:11:19 -0300

    Removes unnecessary code from http-coap interface service

--- http-coap-interface/src/app.py
+++ http-coap-interface/src/app.py
@@ -29,9 +29,8 @@ APP_CONFIG = {
 }
 # Flask App object
 app = Flask(__name__)
+# CORS policy to act over Flask object
 CORS(app)
-# Settings that will be modified by the user
-module_data = {}
 
 
 #########[ Utils ]#############################################################
@@ -64,10 +63,13 @@ def show_resources():
             'url_post' : make_resource_url('execute_coap_request'),
             'url_put'  : make_resource_url('execute_coap_request'),
         },
+        'http_to_coap_test' : {
+            'url_post' : make_resource_url('test_coap_request'),
+            'url_put'  : make_resource_url('test_coap_request'),
+        },
     }
     # return the response with the status code
     return create_json_response(response, 200)
-
 
 
 @app.route(APP_CONFIG["PREFIX"] + '/http_to_coap/test/', methods=['PUT', 'POST'])
@@ -75,7 +77,6 @@ def test_coap_request():
     if not request.json:
         response = generate_invalid_coap_response()
         return create_json_response(response, 422)
-    print(f"execute_coap_request - {request.json}")
     coap_fields = create_coap_fields_from_http_request(**request.json)
     command_response = generate_test_coap_response(**coap_fields)
     return create_json_response(command_response, 200)
@@ -94,6 +95,23 @@ def execute_coap_request():
 #########[ Specific module code ]##############################################
 
 
+def generate_invalid_coap_response(**kwargs):
+    return {
+        "error" : "invalid data received from client",
+        "received" : kwargs,
+        "solution" : "pass correct message request body in JSON format",
+        "request_body_example": {
+            "coap_server_ip" : "192.168.1.37",
+            "coap_server_resource" : "light",
+            "coap_server_port" : 5683,
+            "coap_method" : "put",
+            "coap_payload" : {
+                "light": False
+            }
+        }
+    }
+
+
 def create_coap_fields_from_http_request(**kwargs):
     return {
         "coap_server_ip" : kwargs.get("coap_server_ip", "INVALID"),
@@ -101,6 +119,69 @@ def create_coap_fields_from_http_request(**kwargs):
         "coap_server_port" : int(kwargs.get("coap_server_port", 0)),
         "coap_method" : kwargs.get("coap_method", "INVALID").lower(),
         "coap_payload" : kwargs.get("coap_payload", {}),
+    }
+
+
+def generate_test_coap_response(**kwargs):
+    return {
+        "message" : "Test HTTP-COAP Interface endpoint",
+        "received" : kwargs,
+    }
+
+
+def parse_coap_client_response(command_output):
+    
+    def _process_status_message(message):
+        coap_status_message = {
+            "2.00" : "OK",
+            "2.01" : "Created",
+            "2.02" : "Deleted",
+            "2.03" : "Valid",
+            "2.04" : "Changed",
+            "2.05" : "Content",
+            "2.31" : "Continue",
+            "4.0"  : "Bad Request",
+            "4.01" : "Unauthorized",
+            "4.02" : "Bad Option",
+            "4.03" : "Forbidden",
+            "4.04" : "Not Found",
+            "4.05" : "Method Not Allowed",
+            "4.06" : "Not Acceptable",
+            "4.08" : "Request Entity Incomplt.",
+            "4.12" : "Precondition Failed",
+            "4.13" : "Request Ent. Too Large",
+            "4.15" : "Unsupported Content-Fmt.",
+            "5.00" : "Internal Server Error",
+            "5.01" : "Not Implemented",
+            "5.02" : "Bad Gateway",
+            "5.03" : "Service Unavailable",
+            "5.04" : "Gateway Timeout",
+            "5.05" : "Proxying Not Supported",
+        }
+        status_response = {
+            "coap_status_response": message,
+            "coap_status_description": coap_status_message.get(message, "")
+        }
+        return status_response
+        
+    splitted = command_output.split("\n")
+    try:
+        message_output = json.loads(splitted[1])
+        if isinstance(message_output, (int, float)):
+            message_output = _process_status_message(str(message_output))
+    except:
+        message_output = splitted[1]
+    total_info = splitted[0].split(" ")
+    version = total_info[0].split(":")[1]
+    message_type = total_info[1].split(":")[1]
+    method = total_info[2].split(":")[1]
+    message_id = total_info[3].split(":")[1]
+    return {
+        "version": version,
+        "message_type": message_type,
+        "method": method,
+        "message_id": f"0x{message_id}",
+        "output" : message_output,
     }
 
 
@@ -168,87 +249,8 @@ def execute_coap_client_request(**kwargs):
     return command_response
 
 
-def generate_invalid_coap_response(**kwargs):
-    return {
-        "error" : "invalid data received from client",
-        "received" : kwargs,
-        "solution" : "pass correct message request body in JSON format",
-        "request_body_example": {
-            "coap_server_ip" : "192.168.1.37",
-            "coap_server_resource" : "light",
-            "coap_server_port" : 5683,
-            "coap_method" : "put",
-            "coap_payload" : {
-                "light": False
-            }
-        }
-    }
-
-
-def generate_test_coap_response(**kwargs):
-    return {
-        "message" : "Test HTTP-COAP Interface endpoint",
-        "received" : kwargs,
-    }
-
-
-def parse_coap_client_response(command_output):
-    
-    def _process_status_message(message):
-        coap_status_message = {
-            "2.00" : "OK",
-            "2.01" : "Created",
-            "2.02" : "Deleted",
-            "2.03" : "Valid",
-            "2.04" : "Changed",
-            "2.05" : "Content",
-            "2.31" : "Continue",
-            "4.0"  : "Bad Request",
-            "4.01" : "Unauthorized",
-            "4.02" : "Bad Option",
-            "4.03" : "Forbidden",
-            "4.04" : "Not Found",
-            "4.05" : "Method Not Allowed",
-            "4.06" : "Not Acceptable",
-            "4.08" : "Request Entity Incomplt.",
-            "4.12" : "Precondition Failed",
-            "4.13" : "Request Ent. Too Large",
-            "4.15" : "Unsupported Content-Fmt.",
-            "5.00" : "Internal Server Error",
-            "5.01" : "Not Implemented",
-            "5.02" : "Bad Gateway",
-            "5.03" : "Service Unavailable",
-            "5.04" : "Gateway Timeout",
-            "5.05" : "Proxying Not Supported",
-        }
-        status_response = {
-            "coap_status_response": message,
-            "coap_status_description": coap_status_message.get(message, "")
-        }
-        return status_response
-        
-    splitted = command_output.split("\n")
-    try:
-        message_output = json.loads(splitted[1])
-        if isinstance(message_output, (int, float)):
-            message_output = _process_status_message(str(message_output))
-    except:
-        message_output = splitted[1]
-    total_info = splitted[0].split(" ")
-    version = total_info[0].split(":")[1]
-    message_type = total_info[1].split(":")[1]
-    method = total_info[2].split(":")[1]
-    message_id = total_info[3].split(":")[1]
-    return {
-        "version": version,
-        "message_type": message_type,
-        "method": method,
-        "message_id": f"0x{message_id}",
-        "output" : message_output,
-    }
-
-
 #########[ Module main code ]##################################################
+
 
 def init_app():
     pass
@@ -260,5 +262,6 @@ if __name__ == '__main__':
         port=APP_CONFIG.get("PORT"),
         debug=APP_CONFIG.get("DEBUG"),
         )
+
 
 #########[ Enf of file ]#######################################################
